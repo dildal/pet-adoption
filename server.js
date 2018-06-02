842d2a8b72d278e49db5acadd0f299ddc5793fa7
Michael <mddally@gmail.com>
2018-06-01 20:55:39 -0400

    debugging heroku deploy

--- server.js
+++ server.js
@@ -25,22 +25,11 @@ app.get('/*', function(req,res) {
 	res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// Set our api routes
 
-
-// Catch all other routes and return the index file
-/**
- * Get port from environment and store in Express.
- */
 const port = process.env.PORT || '3000';
-app.set('port', port);
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(port, () => console.log(`API running on localhost:${port}`));
